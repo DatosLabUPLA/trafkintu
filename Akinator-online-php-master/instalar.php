a8c99aba5a67d946b0bdc4d58ed2d0074f8e1a60
AlejandroEG <alejandroespinoza1996@gmail.com>
2022-07-25 09:28:43 -0400

    "akinator" php

---
+++ Akinator-online-php-master/instalar.php
@@ -0,0 +1,44 @@
+<?php
+
+//CONECTAMOS CON LA BD
+require "conexion.php";
+
+//-------------------------------------------------
+$consultas = [];
+
+//PONEMOS LAS CONSULTAS EN UN ARRAY (TABLA)
+array_push($consultas,"CREATE TABLE arbol (nodo INT NOT NULL, texto VARCHAR(500), pregunta BOOL, PRIMARY KEY (nodo));");
+array_push($consultas,"CREATE TABLE partida (id INT NOT NULL AUTO_INCREMENT, personaje VARCHAR(500), acierto BOOL, PRIMARY KEY (id));");
+
+
+//PONEMOS LAS CONSULTAS EN UN ARRAY (DATOS/REGISTROS)
+array_push($consultas,"INSERT INTO arbol (nodo,texto,pregunta) VALUES(1,'Javier Mu�iz', FALSE);");
+
+
+
+//OBTENEMOS EL TAMA�O DEL ARRAY
+$tam = count($consultas);
+
+//-------------------------------------------------
+
+//EJECUTAMOS TODAS LAS CONSULTAS
+
+for($a=0; $a<$tam; $a++){
+	
+	echo "CONSULTA: " . $a . " ";
+	
+	if (mysqli_query($enlace, $consultas[$a])) {
+		echo "OK";
+	}
+	else{
+		echo "ERROR";
+	}
+	
+	echo "<br>";
+}
+
+//-------------------------------------------
+
+/* cerrar la conexi�n */
+mysqli_close($enlace);
+?>
